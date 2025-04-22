59760cc6191c5708ea3a90469c57688783b9fcfd
CHiiii5640 <yitufudiye93@gmail.com>
2025-04-23 00:34:59 +0800

    Initial Commit

---
+++ LibraryCheckInApp/LibraryCheckInAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryCheckInAppApp.swift
+//  LibraryCheckInApp
+//
+//  Created by 劭祈 on 2025/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LibraryCheckInAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LibraryCheckInAppTests/LibraryCheckInAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryCheckInAppTests.swift
+//  LibraryCheckInAppTests
+//
+//  Created by 劭祈 on 2025/4/23.
+//
+
+import Testing
+@testable import LibraryCheckInApp
+
+struct LibraryCheckInAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
